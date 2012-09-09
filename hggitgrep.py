c25104acf37bd124de83c215a87ef2715569637f
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2012-09-09 11:27:44 +0200

    Use proper return code

--- hggitgrep.py
+++ hggitgrep.py
@@ -1,12 +1,14 @@
 import itertools
 import os.path
 import subprocess
+import sys
 
 def gitgrep(ui, repo, what, **opts):
     status = repo.status(clean=True)
     files = list(itertools.chain(status[0], status[1], status[4], status[6]))
-    if files:
-        subprocess.check_call(['git', 'grep', '--no-index', what] + files,
-                              cwd=repo.root)
+    if not files:
+        sys.exit('no files in repo')
+    cmd = ['git', 'grep', '--no-index', what] + files
+    sys.exit(subprocess.call(cmd, cwd=repo.root))
 
 cmdtable = {"gitgrep": (gitgrep, [], "[what]")}
